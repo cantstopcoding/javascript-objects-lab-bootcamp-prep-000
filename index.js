3256fb715adb1239ba742515b90be93bd0e415a3
Jonathan Koss <jonathan_koss@yahoo.com>
2019-06-05 03:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ let recipes = {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
